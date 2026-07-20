9fd0647673d165d0c07818b992c87091f9a17419
shashikant-38 <patilshashikant4508@gmail.com>
2026-07-20 22:36:16 +0530

    update appointment entity

--- src/main/java/com/example/hospitalManagement/Entity/Appointment.java
+++ src/main/java/com/example/hospitalManagement/Entity/Appointment.java
@@ -15,21 +15,21 @@ import java.time.LocalDateTime;
 @ToString
 public class Appointment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
 
-    @Column(nullable = false)
-    private LocalDateTime appointmentTime;
+     @Column(nullable = false)
+     private LocalDateTime appointmentTime;
 
-    @Column(length = 500)
-    private String reason;
+     @Column(length = 500)
+     private String reason;
 
-    @ManyToOne
-    @JoinColumn(name="patient_id" ,nullable=false)
-    private Patient patient;
+     @ManyToOne
+     @JoinColumn(name="patient_id" ,nullable=false)
+     private Patient patient;
 
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    private Doctor doctor;
+     @ManyToOne
+     @JoinColumn(nullable = false)
+     private Doctor doctor;
 }
